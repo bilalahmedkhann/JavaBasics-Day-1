2be20ed3a203c3fd1f2b41d55a402f804516abeb
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2022-11-09 20:48:47 +0530

    ADDED: code to display default value of data type.

---
+++ DefaultValue.java
@@ -0,0 +1,24 @@
+
+    /*Write a Java program to display the default value of all primitive data types of Java*/
+
+
+    public class DefaultValue {
+        int result1;
+        String result2;
+        double result3;
+        float result4;
+        long result5;
+        boolean result6;
+        public void displayMessage() {
+
+
+            System.out.println("Obtained default value result are:  ");
+            System.out.println("Default value of Integer = " + result1);
+            System.out.println("Default value of String = " + result2);
+            System.out.println("Default value of Double = " + result3);
+            System.out.println("Default value of Float = " + result4);
+            System.out.println("Default value of Long = " + result5);
+            System.out.println("Default value of Boolean = " + result6);
+        }
+    }
+
